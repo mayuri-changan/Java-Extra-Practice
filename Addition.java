1cc0555594a2a980d539e01b60c825ae059bdb84
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-19 00:17:14 +0530

    practice

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package com.demo.basics;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("addition of two numbers is:");
+		int n1=10;
+		int n2=20;
+		int add=n1+n2;
+		System.out.println(add);
+
+	}
+
+}
