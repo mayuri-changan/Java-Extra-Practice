a2c3c9695b07bdeed937a2c19b41aeeb93b697aa
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-19 00:18:04 +0530

    practice

---
+++ Division.java
@@ -0,0 +1,15 @@
+package com.demo.basics;
+
+public class Division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("addition of two numbers is:");
+		int n1=5;
+		int n2=2;
+		int div=n1/n2;
+		System.out.println(div);
+
+	}
+
+}
