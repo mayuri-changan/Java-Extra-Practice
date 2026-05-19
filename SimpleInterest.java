a9e4b36bb6c62bf2fee999076ca1d5fa30e48c70
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-20 01:01:32 +0530

    practice

---
+++ SimpleInterest.java
@@ -0,0 +1,16 @@
+package com.demo.basics;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int SI;
+		int p=1000;
+		int r=2;
+		int t = 45;
+		SI=(p*r*t)/100;
+		int amount=SI+p;
+		System.out.println("simple Interset:"+SI+" \n"+"amount is:"+amount);
+	}
+
+}
