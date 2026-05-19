ce0451a3dd4bb399ca27dd145a201eed5d702516
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-20 01:00:51 +0530

    practice

---
+++ NameAgeCity.java
@@ -0,0 +1,13 @@
+package com.demo.basics;
+
+public class NameAgeCity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String name="Mayuri Changan";
+       int age=25;
+       String city="PUNE";
+       System.out.println("\n your Name is:"+name+"\n your age is:" +age+"\n your city is:"+city);
+	}
+
+}
