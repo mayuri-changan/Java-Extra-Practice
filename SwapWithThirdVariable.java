9cd16dde951817ff2b50fde8887627fca401c1b9
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-21 16:53:37 +0530

    practice

---
+++ SwapWithThirdVariable.java
@@ -0,0 +1,20 @@
+package com.demo.basics;
+
+public class SwapWithThirdVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=20;
+		int c;
+		System.out.println("numbers are:" +a+" "+b);
+		System.out.println("swap two numbers:");
+		c=a;
+		a=b;
+		b=c;
+		System.out.println(a+" "+b+" ");
+
+	}
+
+}
