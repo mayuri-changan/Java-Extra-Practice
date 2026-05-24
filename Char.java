3cf74d232ccefc46352baae5776445ae61539346
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-25 00:07:07 +0530

    practice

---
+++ Char.java
@@ -0,0 +1,15 @@
+package com.demo.input;
+import java.util.Scanner;
+@SuppressWarnings("unused")
+public class Char {
+
+	public static void main(StringInput[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter:");
+		char ch=sc.next().charAt(0);
+		System.out.println("========here is character======\n"+ch);
+		
+	
+	}
+
+}
