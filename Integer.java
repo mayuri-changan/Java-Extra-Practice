0266572eb02d83e28313d810a8fcd620850ef16c
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-25 00:07:58 +0530

    practice

---
+++ Integer.java
@@ -0,0 +1,13 @@
+package com.demo.input;
+import java.util.Scanner;
+public class Integer {
+
+	public static void main(StringInput[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter integer");
+		int num=sc.nextInt();
+		System.out.println("number is "+num);
+
+	}
+
+}
