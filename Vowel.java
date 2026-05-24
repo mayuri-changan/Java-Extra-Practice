911ad5adea38779ef714b555b1e14522c1e932e6
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-24 17:24:02 +0530

    practice

---
+++ Vowel.java
@@ -0,0 +1,21 @@
+package com.demo.conditional;
+import java.util.Scanner;
+public class Vowel {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter letter");
+		char ch=sc.next().charAt(0);
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+		{
+			System.out.println("vowel");
+		}
+		else
+		{
+			System.out.println("consonants");
+		}
+		
+
+	}
+
+}
