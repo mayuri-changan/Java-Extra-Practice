4cf1c9ed06b5d053883220d3c7caa03e19afd046
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-26 10:59:57 +0530

    pp

---
+++ EvenNumber.java
@@ -0,0 +1,20 @@
+package com.demo.loops;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+	for(int i=0;i<=100;i++)
+	{
+		if(i%2==0)
+		{
+			System.out.println("even"+i);
+		}
+		else if(i%2!=0)
+		{
+			System.out.println("odd"+i);
+		}
+	}
+
+	}
+
+}
