70acbc7639fae83192e5535caea181e045a08ee1
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-26 10:59:08 +0530

    pp

---
+++ StringInput.java
@@ -0,0 +1,15 @@
+package com.demo.input;
+import java.util.Scanner;
+
+public class StringInput {
+
+	public static void main(java.lang.String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter string");
+		String str=sc.nextLine();
+		System.out.println("=====here is string:===\n"+str);
+		
+         
+	}
+
+}
