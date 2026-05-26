10b62a13525dac91646d2888503960199f54af52
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-26 10:58:48 +0530

    pp

---
+++ Perimeter.java
@@ -0,0 +1,15 @@
+package com.demo.input;
+import java.util.Scanner;
+public class Perimeter {
+
+	public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+System.out.println("enter side");
+int side=sc.nextInt();
+int perimeter=4*side;
+System.out.println(perimeter);
+
+
+	}
+
+}
