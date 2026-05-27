ca44b88e02be3d91a0a08f8eefa35166463a43c0
mayuri-changan <mayuri2001changan@gmail.com>
2026-05-27 23:01:37 +0530

    ReverseNumber.java

---
+++ ReverseNumber.java
@@ -0,0 +1,20 @@
+package com.demo.loops;
+import java.util.Scanner;
+public class ReverseNumber{
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+	System.out.println("enter num");
+	int rem;
+	int n=sc.nextInt();
+	int rev=0;
+	while(n!=0)
+	{ 
+		rem=n%10;
+		rev=rev*10+rem;
+		n=n/10;
+	}
+	System.out.println(rev);
+	}
+}
